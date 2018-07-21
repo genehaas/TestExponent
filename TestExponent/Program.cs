56c51c1aa0c113f8032ce7e1f8caa173ca3d902b
Gene Haas <genehaas@outlook.com>
2018-07-21 16:26:53 -0500

    Add project files.

---
+++ TestExponent/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestExponent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2;
+            float y = 3.14F;
+
+            float result = (float)Math.Pow(y, x);
+
+            Console.WriteLine(String.Format("{0} raised to {1} power = {2}.", y, x, result));
+            Console.ReadLine();
+        }
+    }
+}
